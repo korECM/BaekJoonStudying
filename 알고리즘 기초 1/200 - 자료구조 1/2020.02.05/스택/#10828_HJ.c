12a1ff563124da2b2be728bd16a24d1087444cdc
dlguswn3659 <39727494+dlguswn3659@users.noreply.github.com>
2020-02-08 01:33:10 +0900

    Update #10828_HJ.c

--- 알고리즘 기초 1/200 - 자료구조 1/2020.02.05/스택/#10828_HJ.c
+++ 알고리즘 기초 1/200 - 자료구조 1/2020.02.05/스택/#10828_HJ.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <string>
+#include <iostream>
+using namespace std;
 
 #define MAX_STACK_SIZE 10001
 #define MAX_INT 100001
 
-char* stack[MAX_STACK_SIZE];
+string stack[MAX_STACK_SIZE];
 char order[MAX_STACK_SIZE][15] = {{'N'}};
 
 
@@ -27,7 +30,7 @@ int IsFull(){
     }
 }
 
-void push(char* value){
+void push(string value){
     if(IsFull() == 1){
         printf("Stack is full!");
     } else {
@@ -35,7 +38,7 @@ void push(char* value){
     }
 }
 
-char* pop(){
+string pop(){
     if(IsEmpty() == 1){
         return "-1";
     } else {
@@ -43,7 +46,7 @@ char* pop(){
     }
 }
 
-char* top_print(){
+string top_print(){
     if(IsEmpty() == 1){
         return "-1";
     } else {
@@ -70,31 +73,47 @@ int main(){
         //printf("%d\n", strcmp(order[i], (char *)"top"));
     }
     
-    char push_value[6] = {' '};
-    
+    //char push_value[6] = {' '};
+    string push_value;
     
     for(int i = 0; i < order_size; i++){
+        int index_count = 5;
+        
         if(!strncmp(order[i], "top", 3)){
-            printf("%s\n", top_print());
+            // printf("%s\n", top_print());
+            // printf("top %d\n", i);
+            if(IsEmpty() == 1){
+                printf("-1\n");
+            } else {
+                printf("%s\n", stack[top].c_str());
+            }
+           //cout << top_print();
         }
         else if(!strncmp(order[i], "size", 3)){
+            //printf("size\n");
             printf("%d\n", top + 1);
         }
         else if(!strncmp(order[i], "empty", 3)){
+            //printf("empty\n");
             printf("%d\n", IsEmpty());
         }
         else if(!strncmp(order[i], "pop", 3)){
-            printf("%s\n", pop());
+            //printf("%s\n", pop());
+            //printf("pop\n");
+            cout << pop();
         }
         else if(!strncmp(order[i], "push", 3)){
-            for(int j = 5; order[i][j] != 'N'; j++){
-                push_value[j-5] = order[i][j];
-                //if(order[i][j + 1] == 'N'){
-                //    break;
-                //}
+            for(int j = 5; j < 15; j++){
+                //printf("%c", push_value[j - 5]);
+                index_count++;
+                //printf("%d\n", index_count);
+                if(order[i][j + 1] == 'N'){
+                    break;
+                }
+                
             }
-            //push_value = "123";
-            //printf("%s", push_value);
+            push_value.append(order[i], 5, index_count);
+            
             push(push_value);
         }
     }
