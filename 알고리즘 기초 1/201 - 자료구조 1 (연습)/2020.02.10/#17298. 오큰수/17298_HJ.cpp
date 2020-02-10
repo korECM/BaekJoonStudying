f31cb3f240bfb98ddddee709a604e85ca6ceaed3
hyunju <dlguswn3659@naver.com>
2020-02-11 03:26:12 +0900

    17298

--- 알고리즘 기초 1/201 - 자료구조 1 (연습)/2020.02.10/#17298. 오큰수/17298_HJ.cpp
+++ 알고리즘 기초 1/201 - 자료구조 1 (연습)/2020.02.10/#17298. 오큰수/17298_HJ.cpp
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stack>
 #define MAX_STACK_SIZE 1000001
-
-using namespace std;
 
 int bigger(int a, int b){
   if(a > b) return a;
@@ -12,28 +9,47 @@ int bigger(int a, int b){
 
 int main(){
   int value_arr[MAX_STACK_SIZE] = {0};
-  int F_arr[MAX_STACK_SIZE] = {0};
+  int nge[MAX_STACK_SIZE] = {0};
   int biggest_value = 0;
-  int stack_size = 0;
   int value = 0;
-  stack<int>s;
+  int stack_size = 0;
 
-  cin >> stack_size;
+  scanf("%d", &stack_size);
 
-  for(int i = 0; i < stack_size; i++){
+  for(int i = 1; i < stack_size + 1; i++){
     scanf("%d", &value_arr[i]);
   }
 
-  for(int i = 0; i < stack_size; i++){
+  for(int i = 1; i < stack_size + 1; i++){
     biggest_value = bigger(biggest_value, value_arr[i]);
   }
 
-  for(int i = 0; i < stack_size; i++){
-    for(int j = 0; j < biggest_value; j++){
-      printf("무언가 잘못되고 있음.\n", );
+  for(int i = 1; i < stack_size + 1; i++){
+    if(value_arr[i] == biggest_value){
+      nge[i] = -1;
+    }else if(value_arr[i + 1] == 0){
+        nge[i] = -1;
     }
   }
 
+  for(int i = 1; i < stack_size + 1; i++){
+    if(nge[i] == 0){  //할당된 애가 아니면
+      for(int j = i + 1; j < stack_size + 1; j++){
+        if(nge[j] == -1){
+            nge[i] = value_arr[j];
+            break;
+        }
+        else if(value_arr[i] < value_arr[j]){
+            nge[i] = value_arr[j];
+            break;
+        }
+      }
+    }
+  }
+
+  for(int i = 1; i < stack_size + 1; i++){
+    printf("%d ", nge[i]);
+  }
 
   return 0;
 }
