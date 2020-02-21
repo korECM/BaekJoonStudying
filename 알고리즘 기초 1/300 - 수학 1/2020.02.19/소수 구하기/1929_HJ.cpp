879adaae6741ddad9105af22173ecac8e558aee5
hyunju <dlguswn3659@naver.com>
2020-02-22 02:05:23 +0900

    1929_HJ solve

--- 알고리즘 기초 1/300 - 수학 1/2020.02.19/소수 구하기/1929_HJ.cpp
+++ 알고리즘 기초 1/300 - 수학 1/2020.02.19/소수 구하기/1929_HJ.cpp
@@ -1,49 +1,30 @@
 #include <stdio.h>
-#define MAX_SIZE 1000001
-
-long long gcd(long long a, long long b){
-	while(b!=0){
-		int r = a%b;
-		a= b;
-		b= r;
-	}
-	return a;
-}
-
-long long lcm(long long a, long long b){
-    return a * b / gcd(a,b);
-}
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
 int main(){
-  int start_num, end_num;
-  scanf("%d %d", &start_num, &end_num);
-  printf("%d %d\n", start_num, end_num);
+    int begin, end;
+    scanf("%d %d",&begin, &end);
 
-  int total = -1;
-  int prime[MAX_SIZE];
-  for(int i = start_num; i < end_num + 1; i++){
-    total++;
-    prime[total] = i;
-    //printf("%d\n", prime[total]);
-    if(i > 1){
-      for(int j = 2; j < i; j++){
-        if(gcd(j, i) != 1){
-          prime[total] = 0;
-          total--;
-          break;
+    bool *c = new bool[end+1];
+
+    c[1] = true;
+    
+    for(int i = 2; i*i<= end; i++){
+        if(c[i] == false){
+            for(int j = i * i; j <= end; j+=i){
+                c[j] = true;
+            }
         }
+    }
+
+    for(int i = begin; i <= end; i++){
+      if(c[i] == false){
+        printf("%d\n",i);
       }
-    }else if(i == 1) prime[total] = 0; total--;
-  }
-
-  for(int i = 0; i < MAX_SIZE; i++){
-    if(prime[i] > 0){
-      printf("%d\n", prime[i]);
     }
-    else{
-      break;
-    }
-  }
 
-  return 0;
+    delete c;
+    return 0;
 }
