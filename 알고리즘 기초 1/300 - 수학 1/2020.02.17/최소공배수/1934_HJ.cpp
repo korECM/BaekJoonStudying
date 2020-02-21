15d90c1c6f404f3cb58caf7120ea8d6d9d9d3c90
hyunju <dlguswn3659@naver.com>
2020-02-21 15:52:22 +0900

    1934_HJ solve

---
+++ 알고리즘 기초 1/300 - 수학 1/2020.02.17/최소공배수/1934_HJ.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define MAX_SIZE 1001
+
+long long gcd(long a, long b){
+	while(b!=0){
+		int r = a%b;
+		a= b;
+		b= r;
+	}
+	return a;
+}
+
+long long lcm(long long a, long long b){
+    return a * b / gcd(a,b);
+}
+
+int main(){
+  int order_num;
+  scanf("%d", &order_num);
+
+  long long test_case[MAX_SIZE][2];
+  long long tmp[MAX_SIZE];
+
+  for(int i = 0; i < order_num; i++){
+    scanf("%lld %lld", &test_case[i][0], &test_case[i][1]);
+  }
+
+  for(int i = 0; i < order_num; i++){
+    tmp[i] = lcm(test_case[i][0], test_case[i][1]);
+  }
+
+  for(int i = 0; i < order_num; i++){
+    printf("%lld\n", tmp[i]);
+  }
+
+	return 0;
+}
