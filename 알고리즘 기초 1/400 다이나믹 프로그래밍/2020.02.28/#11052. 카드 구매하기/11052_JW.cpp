413074389b758bb392a88d2194cc46bd494dc5c6
JongwonYoun <jeffyoun@naver.com>
2020-02-28 00:06:46 +0900

    11052_JW solve

---
+++ 알고리즘 기초 1/400 다이나믹 프로그래밍/2020.02.28/#11052. 카드 구매하기/11052_JW.cpp
@@ -0,0 +1,52 @@
+#include <string.h>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int n;
+
+int mem[1001][10001];
+
+int solve(vector<int>& input, int remain, int index) {
+    if (index == 1)
+        return remain * input[1];
+    if (remain == 0)
+        return 0;
+    if (remain < 0)
+        return -987654321;
+
+    int& ret = mem[remain][index];
+    if (ret != -1)
+        return ret;
+
+    int maximum = -987654321;
+    maximum =
+        max(maximum, solve(input, remain - index, index) + input[index]);
+    maximum = max(maximum, solve(input, remain, index - 1));
+    return ret = maximum;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input1.txt", "r", stdin);
+#endif
+
+    cin >> n;
+    vector<int> input(n + 1);
+    memset(mem, -1, sizeof(mem));
+    for (int i = 1; i <= n; i++) {
+        cin >> input[i];
+    }
+
+    cout << solve(input, n, n) << endl;
+
+#ifndef ONLINE_JUDGE
+    fclose(stdin);
+#endif
+
+    return 0;
+}
