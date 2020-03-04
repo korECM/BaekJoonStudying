7307fecd1abf0cbdd66a8e6a085f5dfe5d909576
JongwonYoun <jeffyoun@naver.com>
2020-03-05 01:12:06 +0900

    5337_JW solve

---
+++ 그냥 문제 푸는 폴더/#5337. 웰컴/5337_JW.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cout << ".  .   ." << endl;
+    cout << "|  | _ | _. _ ._ _  _" << endl;
+    cout << "|/\\|(/.|(_.(_)[ | )(/." << endl;
+
+#ifndef ONLINE_JUDGE
+    fclose(stdin);
+#endif
+
+    return 0;
+}
+
+/*
+.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.
+
+
+
+*/
