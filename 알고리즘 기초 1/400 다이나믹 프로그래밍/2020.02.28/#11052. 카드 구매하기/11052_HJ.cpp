a8fdbd9ff54259b473e0467f1dd64cd549774d36
hyunju <dlguswn3659@naver.com>
2020-03-07 04:07:39 +0900

    11052_HJ solve

---
+++ 알고리즘 기초 1/400 다이나믹 프로그래밍/2020.02.28/#11052. 카드 구매하기/11052_HJ.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 1001
+
+int main() {
+    int cardNum;
+    scanf("%d", &cardNum);
+
+    int cardPrice[MAX_SIZE];
+
+    for(int i = 1 ; i < cardNum + 1; i++){
+        scanf("%d", &cardPrice[i]);
+    }
+
+    int tmp = 1;
+    int buyingCard = 0;
+    long long payMoney = 0;
+
+    while(buyingCard != cardNum){
+        tmp = 1;
+        for(int i = 2; i < cardNum + 1; i++){
+            if((cardNum - buyingCard >= i) && (((double)cardPrice[i] / (double)i) > ((double)cardPrice[tmp] / (double)(tmp)))){
+                tmp = i;
+            }else if(cardNum - buyingCard < i){
+                break;
+            }
+        }
+        payMoney = payMoney + cardPrice[tmp];
+        buyingCard = buyingCard + tmp;
+    }
+
+    printf("%lld", payMoney);
+
+    return 0;
+}
