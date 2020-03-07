547a67e61213fc669bd4eb5d35e14eb4c7cc43ca
JongwonYoun <jeffyoun@naver.com>
2020-03-07 23:48:52 +0900

    1932_JW init

---
+++ 알고리즘 기초 1/401 - 다이나믹 프로그래밍 1 (연습)/2020.03.07/#1932. 정수 삼각형/1932_JW.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+#endif
+
+#ifdef LOCAL
+    fclose(stdin);
+#endif
+
+    return 0;
+}
