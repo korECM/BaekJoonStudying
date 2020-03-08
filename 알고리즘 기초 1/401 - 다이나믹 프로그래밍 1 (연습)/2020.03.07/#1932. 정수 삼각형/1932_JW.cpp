b1350f05b33f0c9061f15ea3dd571fd5b0f9ac10
JongwonYoun <jeffyoun@naver.com>
2020-03-08 16:05:12 +0900

    1932_JW solve

--- 알고리즘 기초 1/401 - 다이나믹 프로그래밍 1 (연습)/2020.03.07/#1932. 정수 삼각형/1932_JW.cpp
+++ 알고리즘 기초 1/401 - 다이나믹 프로그래밍 1 (연습)/2020.03.07/#1932. 정수 삼각형/1932_JW.cpp
@@ -7,10 +7,32 @@
 
 using namespace std;
 
+vector<vector<int>> input(500, vector<int>(500));
+int map[500][500];
+int n;
+
+int solve(int x, int y) {
+    if (x >= n || y >= n)
+        return 0;
+    int& ret = map[y][x];
+    if (ret != -1)
+        return ret;
+    return ret = (max(solve(x, y + 1), solve(x + 1, y + 1)) + input[y][x]);
+}
+
 int main() {
 #ifdef LOCAL
     freopen("input.txt", "r", stdin);
 #endif
+    memset(map, -1, sizeof(map));
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cin >> input[i][j];
+        }
+    }
+
+    cout << solve(0, 0) << endl;
 
 #ifdef LOCAL
     fclose(stdin);
