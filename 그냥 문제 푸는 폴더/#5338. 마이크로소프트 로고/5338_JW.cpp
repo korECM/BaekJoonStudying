33257ce8fe752f1463b4387abc1a1d709a8bc035
JongwonYoun <jeffyoun@naver.com>
2020-03-09 19:22:36 +0900

    5338_JW solve

---
+++ 그냥 문제 푸는 폴더/#5338. 마이크로소프트 로고/5338_JW.cpp
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cout << "       _.-;;-._" << endl;
+    cout << "'-..-'|   ||   |" << endl;
+    cout << "'-..-'|_.-;;-._|" << endl;
+    cout << "'-..-'|   ||   |" << endl;
+    cout << "'-..-'|_.-''-._|" << endl;
+
+#ifdef LOCAL
+    fclose(stdin);
+#endif
+
+    return 0;
+}
