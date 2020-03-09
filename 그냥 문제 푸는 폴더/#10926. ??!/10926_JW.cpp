898874b5eac29c92b71e29582bf78f9b86f1f5f9
JongwonYoun <jeffyoun@naver.com>
2020-03-09 19:12:39 +0900

    10926_JW solve

---
+++ 그냥 문제 푸는 폴더/#10926. ??!/10926_JW.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <cmath>
+
+using namespace std;
+
+
+int main() {
+// #ifdef LOCAL
+//     freopen("input.txt", "r", stdin);
+// #endif
+
+    string input;
+    cin >> input;
+    cout << input << "??!" << endl;
+
+// #ifdef LOCAL
+//     fclose(stdin);
+// #endif
+
+    return 0;
+}
