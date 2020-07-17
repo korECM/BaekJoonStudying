40da92d6271a253df04ac271a5f78d5a51873b1b
JongwonYoun <jeffyoun@naver.com>
2020-07-17 21:11:51 +0900

    1번 파일 추가

---
+++ testFolder/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <cmath>
+
+using namespace std;
+
+
+int main() {
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+#endif
+
+
+
+#ifdef LOCAL
+    fclose(stdin);
+#endif
+
+    return 0;
+}
